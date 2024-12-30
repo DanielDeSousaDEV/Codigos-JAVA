c491f5e59963c3c9f59e41ea8acc4486c9ccdd64
Daniel de Sousa <156535206+DanielDeSousaDEV@users.noreply.github.com>
2024-12-30 09:46:48 -0300

    Atualizar o Main.java

--- Calculadora/Main.java
+++ Calculadora/Main.java
@@ -1,1 +1,8 @@
-a
+public class Main{
+  public static void main(String args[]){
+    Calculator calc = new Calculator();
+    calc.captureInformations();
+    calc.testOperator();
+    calc.show();
+  }
+}
